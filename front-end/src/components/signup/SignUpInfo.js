bb7b5b882e6a77438030c09ac6994556bf9e3f3c
david.t.wayne <david.t.wayne.ctr@usmc.mil>
2021-11-06 13:47:08 -0400

    test sign up

--- front-end/src/components/signup/SignUpInfo.js
+++ front-end/src/components/signup/SignUpInfo.js
@@ -1,89 +1,163 @@
-import React from 'react';
-import "./SignUpInfo.css"
+import React from "react";
+import "./SignUpInfo.css";
+import { useState } from "react";
+import { gql, useMutation } from '@apollo/client';
+
+const Signup_MUTATION = gql`
+  mutation Signup($firstName: String, $lastName: String, $address: String, $city: String, $state: String, $zipCode: String) {
+    signUp(firstName: $firstName, lastName: $lastName, address: $address, city: $city, state: $state, zipCode: $zipCode) {
+      id
+    }
+  }
+`;
 
 function SignUpInfo() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [zipCode, setZipCode] = useState("");
+
+  const [signupMutation] = useMutation(Signup_MUTATION);
+
+  function signup() {
+  debugger
+  signupMutation({ variables: { firstName, lastName, address, city, state, zipCode } })
+  .then((result) => console.log(result));
+  }
+
   return (
     <div>
       <form className="contact-form">
         <h2 className="contact-title">Contact Info</h2>
 
-        <label className="first-name-label" htmlFor="firstName">First Name</label>
-        <input className="first-name-input field-input" id="firstName" type='text' />
+        <label className="first-name-label" htmlFor="firstName">
+          First Name
+        </label>
+        <input
+          className="first-name-input field-input"
+          id="firstName"
+          type="text"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
 
-        <label className="last-name-label" htmlFor="lastName">Last Name</label>
-        <input className="last-name-input field-input" id="lastName" type='text' />
+        <label className="last-name-label" htmlFor="lastName">
+          Last Name
+        </label>
+        <input
+          className="last-name-input field-input"
+          id="lastName"
+          type="text"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
 
-        <label className="address-label" htmlFor="address">Address</label>
-        <input className="address-input field-input" id="address" type='text' />
+        <label className="address-label" htmlFor="address">
+          Address
+        </label>
+        <input
+          className="address-input field-input"
+          id="address"
+          type="text"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
 
-        <label className="city-label" for="city">City</label>
-        <input className="city-input field-input" id="city" type='text' />
+        <label className="city-label" for="city">
+          City
+        </label>
+        <input
+          className="city-input field-input"
+          id="city"
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
 
-          <label className="state-label" for='state'>State</label>
-          <select className="state-input field-input" id='state' name='state'>
-            <option value='---'>---</option>
-            <option value='Alabama'>Alabama</option>
-            <option value='Alaska'>Alaska</option>
-            <option value='Arizona'>Arizona</option>
-            <option value='Arkansas'>Arkansas</option>
-            <option value='California'>California</option>
-            <option value='Colorado'>Colorado</option>
-            <option value='Connecticut'>Connecticut</option>
-            <option value='Delaware'>Delaware</option>
-            <option value='District of Columbia'>District of Columbia</option>
-            <option value='Florida'>Florida</option>
-            <option value='Georgia'>Georgia</option>
-            <option value='Guam'>Guam</option>
-            <option value='Hawaii'>Hawaii</option>
-            <option value='Idaho'>Idaho</option>
-            <option value='Illinois'>Illinois</option>
-            <option value='Indiana'>Indiana</option>
-            <option value='Iowa'>Iowa</option>
-            <option value='Kansas'>Kansas</option>
-            <option value='Kentucky'>Kentucky</option>
-            <option value='Louisiana'>Louisiana</option>
-            <option value='Maine'>Maine</option>
-            <option value='Maryland'>Maryland</option>
-            <option value='Massachusetts'>Massachusetts</option>
-            <option value='Michigan'>Michigan</option>
-            <option value='Minnesota'>Minnesota</option>
-            <option value='Mississippi'>Mississippi</option>
-            <option value='Missouri'>Missouri</option>
-            <option value='Montana'>Montana</option>
-            <option value='Nebraska'>Nebraska</option>
-            <option value='Nevada'>Nevada</option>
-            <option value='New Hampshire'>New Hampshire</option>
-            <option value='New Jersey'>New Jersey</option>
-            <option value='New Mexico'>New Mexico</option>
-            <option value='New York'>New York</option>
-            <option value='North Carolina'>North Carolina</option>
-            <option value='North Dakota'>North Dakota</option>
-            <option value='Northern Marianas Islands'>
-              Northern Marianas Islands
-            </option>
-            <option value='Ohio'>Ohio</option>
-            <option value='Oklahoma'>Oklahoma</option>
-            <option value='Oregon'>Oregon</option>
-            <option value='Pennsylvania'>Pennsylvania</option>
-            <option value='Puerto Rico'>Puerto Rico</option>
-            <option value='Rhode Island'>Rhode Island</option>
-            <option value='South Carolina'>South Carolina</option>
-            <option value='South Dakota'>South Dakota</option>
-            <option value='Tennessee'>Tennessee</option>
-            <option value='Texas'>Texas</option>
-            <option value='Utah'>Utah</option>
-            <option value='Vermont'>Vermont</option>
-            <option value='Virginia'>Virginia</option>
-            <option value='Virgin Islands'>Virgin Islands</option>
-            <option value='Washington'>Washington</option>
-            <option value='West Virginia'>West Virginia</option>
-            <option value='Wisconsin'>Wisconsin</option>
-            <option value='Wyoming'>Wyoming</option>
-          </select>
+        <label className="state-label" for="state">
+          State
+        </label>
+        <select
+          className="state-input field-input"
+          id="state"
+          name="state"
+          value={state}
+          onChange={(e) => setState(e.target.value)}
+        >
+          <option value="---">---</option>
+          <option value="Alabama">Alabama</option>
+          <option value="Alaska">Alaska</option>
+          <option value="Arizona">Arizona</option>
+          <option value="Arkansas">Arkansas</option>
+          <option value="California">California</option>
+          <option value="Colorado">Colorado</option>
+          <option value="Connecticut">Connecticut</option>
+          <option value="Delaware">Delaware</option>
+          <option value="District of Columbia">District of Columbia</option>
+          <option value="Florida">Florida</option>
+          <option value="Georgia">Georgia</option>
+          <option value="Guam">Guam</option>
+          <option value="Hawaii">Hawaii</option>
+          <option value="Idaho">Idaho</option>
+          <option value="Illinois">Illinois</option>
+          <option value="Indiana">Indiana</option>
+          <option value="Iowa">Iowa</option>
+          <option value="Kansas">Kansas</option>
+          <option value="Kentucky">Kentucky</option>
+          <option value="Louisiana">Louisiana</option>
+          <option value="Maine">Maine</option>
+          <option value="Maryland">Maryland</option>
+          <option value="Massachusetts">Massachusetts</option>
+          <option value="Michigan">Michigan</option>
+          <option value="Minnesota">Minnesota</option>
+          <option value="Mississippi">Mississippi</option>
+          <option value="Missouri">Missouri</option>
+          <option value="Montana">Montana</option>
+          <option value="Nebraska">Nebraska</option>
+          <option value="Nevada">Nevada</option>
+          <option value="New Hampshire">New Hampshire</option>
+          <option value="New Jersey">New Jersey</option>
+          <option value="New Mexico">New Mexico</option>
+          <option value="New York">New York</option>
+          <option value="North Carolina">North Carolina</option>
+          <option value="North Dakota">North Dakota</option>
+          <option value="Northern Marianas Islands">
+            Northern Marianas Islands
+          </option>
+          <option value="Ohio">Ohio</option>
+          <option value="Oklahoma">Oklahoma</option>
+          <option value="Oregon">Oregon</option>
+          <option value="Pennsylvania">Pennsylvania</option>
+          <option value="Puerto Rico">Puerto Rico</option>
+          <option value="Rhode Island">Rhode Island</option>
+          <option value="South Carolina">South Carolina</option>
+          <option value="South Dakota">South Dakota</option>
+          <option value="Tennessee">Tennessee</option>
+          <option value="Texas">Texas</option>
+          <option value="Utah">Utah</option>
+          <option value="Vermont">Vermont</option>
+          <option value="Virginia">Virginia</option>
+          <option value="Virgin Islands">Virgin Islands</option>
+          <option value="Washington">Washington</option>
+          <option value="West Virginia">West Virginia</option>
+          <option value="Wisconsin">Wisconsin</option>
+          <option value="Wyoming">Wyoming</option>
+        </select>
 
-        <label className="zipcode-label" htmlFor="zipcode">Zipcode</label>
-        <input className="zipcode-input field-input" type='text' id="zipcode" />
+        <label className="zipcode-label" htmlFor="zipcode">
+          Zipcode
+        </label>
+        <input
+          className="zipcode-input field-input"
+          type="text"
+          id="zipcode"
+          value={zipCode}
+          onChange={(e) => setZipCode(e.target.value)}
+        />
       </form>
+      <button type="button" onClick={signup}>Signup</button>
     </div>
   );
 }
