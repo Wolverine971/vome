b7de1e09e1d091b2cfcc4033cdad99cbeb1f2022
david.t.wayne <david.t.wayne.ctr@usmc.mil>
2021-11-07 21:24:04 -0500

    bugfix for multiple points showing up

--- front-end/src/components/map/map.js
+++ front-end/src/components/map/map.js
@@ -1,29 +1,26 @@
-import React, { useRef, useEffect, useState } from "react";
-import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
-import "mapbox-gl/dist/mapbox-gl.css";
+import { gql } from "@apollo/client";
 // import env from "react-dotenv";
 import axios from "axios";
-import "./map.css";
 import * as L from "leaflet";
+import mapboxgl from "mapbox-gl";
+import React, { useEffect, useRef, useState } from "react";
 
 import { client } from "../../apollo";
-import { gql } from "@apollo/client";
-
-import house from "../../icons/house2.svg";
-import food from "../../icons/food.svg";
-
 import crayon from "../../icons/crayons.svg";
+import food from "../../icons/food.svg";
+import house from "../../icons/house2.svg";
 import VA from "../../icons/va.svg";
+
+import "./map.css";
+// eslint-disable-line import/no-webpack-loader-syntax
+import "mapbox-gl/dist/mapbox-gl.css";
 
 require("dotenv").config();
 // const key = process.env.MAP_KEY;
 const key =
   "pk.eyJ1IjoiZGp3YXluZTMiLCJhIjoiY2t1b2Q2N2xtMmVsYTJ4bXh5MTVna2kyMiJ9.qs9ffyy-AcnWcLUgEJNO_w";
-console.log(key);
 mapboxgl.accessToken = key;
 const Map = (props) => {
-  //     const key2 = process.env.MAP_KEY;
-  // console.log(key2)
   const { selectedState } = props;
   const mapContainer = useRef(null);
   const map = useRef(null);
@@ -57,7 +54,7 @@ const Map = (props) => {
         ).addTo(map.current);
         map.current.fitBounds(polygon.getBounds());
         polygon.on("click", function (e) {
-          GetServices(e);
+          getServices(e);
         });
         // load state on map
       } else {
@@ -68,14 +65,13 @@ const Map = (props) => {
           )
           .then((resp) => {
             if (resp && resp.data) {
-              try{
+              try {
                 window.localStorage.setItem(
                   selectedState,
                   JSON.stringify(resp.data)
                 );
-              }
-              catch(e){
-                console.log(e)
+              } catch (e) {
+                console.log(e);
               }
               var polygon = L.polygon(
                 recurseArr(resp.data.features[0].geometry.coordinates),
@@ -84,7 +80,7 @@ const Map = (props) => {
               map.current.fitBounds(polygon.getBounds());
 
               polygon.on("click", function (e) {
-                GetServices(e);
+                getServices(e);
               });
             }
           });
@@ -140,14 +136,14 @@ const Map = (props) => {
     });
   }
 
-  async function GetServices(e) {
+  async function getServices(e) {
     client
       .query({
         query: gql`
           query GetServicesInState($state: String, $cursorId: String) {
             getServicesInState(state: $state, cursorId: $cursorId) {
               services {
-                  id
+                id
                 name
                 category
                 description
@@ -166,42 +162,39 @@ const Map = (props) => {
           cursorId: "",
         },
       })
-      .then(async(result) => {
-        console.log(result);
-        if (
-          result
-        ) {
-          console.log(result.data.getServicesInState.services)
-          
-          let markerPromises = []
-          let servicesArr = result.data.getServicesInState.services
+      .then(async (result) => {
+        if (result) {
+
+          let servicesArr = result.data.getServicesInState.services;
           // servicesArr.forEach((service) => {
 
-            for (let i = 0; i < servicesArr.length; i++) { 
-              let service = servicesArr[i]
-              let marker = L.marker(service.coordinates, {
-                title: service.name,
-                icon: service.category === "VA Center" ? VAIcon : (service.category === "Food") ? foodIcon : houseIcon,
-              })
-                .bindPopup(
-                  `<div class="card card-1"><h1 class="card-title">${service.name}</h1><h2 class="card-subtitle">${service.description}</h2><h3>${service.address}, ${service.city}, ${service.state} </h3>
+          for (let i = 0; i < servicesArr.length; i++) {
+            let service = servicesArr[i];
+            let marker = L.marker(service.coordinates, {
+              title: service.name,
+              icon:
+                service.category === "VA Center"
+                  ? VAIcon
+                  : service.category === "Food"
+                  ? foodIcon
+                  : houseIcon,
+            })
+              .bindPopup(
+                `<div class="card card-1"><h1 class="card-title">${service.name}</h1><h2 class="card-subtitle">${service.description}</h2><h3>${service.address}, ${service.city}, ${service.state} </h3>
                   <div>
                   
                   </div>
                   </div>`
-                  // add delete button if we need to delete some items
-                  // <button class="btn" type="button" onClick="deleteItem(event)" id="${service.id}">Delete</button>
+                // add delete button if we need to delete some items
+                // <button class="btn" type="button" onClick="deleteItem(event)" id="${service.id}">Delete</button>
+              )
+              .addTo(map.current);
+            // markerPromises.push(marker)
 
-                )
-                .addTo(map.current)
-                // markerPromises.push(marker)
-                
-
-                
-              marker.on("click", function (e) {
-                console.log(e);
-              });
-            }
+            marker.on("click", function (e) {
+              console.log(e);
+            });
+          }
           // });
           // Promise.all(markerPromises).then(createdMarkers=>{
           //   createdMarkers.forEach(marker=>{
@@ -210,10 +203,6 @@ const Map = (props) => {
           // })
         }
       });
-  }
-
-  function deleteItem(e){
-      console.log(e)
   }
 
   return (
